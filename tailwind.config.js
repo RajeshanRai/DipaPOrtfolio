c0086940e3d27b94db64f7a91406c415c1011c6d
Rajeshan Rai <yellowtown85@gmail.com>
2026-05-22 09:01:28 -0700

    uploading initial code

--- tailwind.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  content: ['./templates/**/*.html', './apps/**/templates/**/*.html', './static_src/**/*.{js,css}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
---
+++ tailwind.config.js.bak
@@ -0,0 +1,7 @@
+module.exports = {
+  content: ['./templates/**/*.html', './apps/**/templates/**/*.html', './static_src/**/*.{js,css}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
